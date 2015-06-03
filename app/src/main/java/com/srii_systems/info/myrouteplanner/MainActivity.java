f27841ebb3b38402ab35f5e14096ae477691f621
blueiqbal <miqbal123@gmail.com>
2015-06-03 13:51:39 +0300

    Just testing how it works

--- app/src/main/java/com/srii_systems/info/myrouteplanner/MainActivity.java
+++ app/src/main/java/com/srii_systems/info/myrouteplanner/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Testing
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
